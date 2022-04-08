31a8220db7403fd254c6f5617b234a08759191c4
fares takorabt <farestakorabt@gmail.com>
2022-04-08 12:18:39 +0200

    Updates

---
+++ precache-manifest.c2711c67e1933605dfc7b7a222d49451.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63822f4e5574dbbc91307fcb0684ad27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9696fa4af7739f44b19a",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "68c9bb1b9535deb04152",
+    "url": "/static/css/main.2470b26e.chunk.css"
+  },
+  {
+    "revision": "9696fa4af7739f44b19a",
+    "url": "/static/js/2.8229afcc.chunk.js"
+  },
+  {
+    "revision": "45783aa1d1e8391ffc7dd0ed296e05bd",
+    "url": "/static/js/2.8229afcc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68c9bb1b9535deb04152",
+    "url": "/static/js/main.4641f413.chunk.js"
+  },
+  {
+    "revision": "ddea8645f36b1e6ed060",
+    "url": "/static/js/runtime-main.74157b77.js"
+  }
+]);
